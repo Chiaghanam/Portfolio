011f7ff69a5a956d0b905c2801b3a8ff1d9a6020
Amobi <leofazzy98@gmail.com>
2026-03-06 11:30:10 +0100

    corrected directory typo, App.css

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../app.css'
+import '../App.css'
 const Contact = ({id}) => {
   return (
     <div id={id}>
--- Portfolio/src/page/ViewPage.jsx
+++ Portfolio/src/page/ViewPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../app.css';
+import '../App.css';
 import NavbarComponent from '../components/NavbarComponent';
 import Home from '../components/Home';
 import About from '../components/About';
